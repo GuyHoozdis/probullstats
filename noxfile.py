e98bf08ed8b17a57dae908a2241ad2bf7615b78c
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-21 15:41:24 -0500

    fix(dev-tools): Added configurations for mypy and ruff

--- noxfile.py
+++ noxfile.py
@@ -10,6 +10,7 @@ nox.options.error_on_external_run = True
 # nox.options.sessions = "lint", "mypy", "rtfd", "xdoctests", "testsuite", "acceptance-tests"
 
 
+SUPPORTED_PYTHON_VERSIONS = ["3.12", "3.11", "3.10", "3.9", "3.8"]
 DEFAULT_PYTHON_VERSION = "3.11"
 SOURCE_CODE_TARGETS = [
     "src/",
@@ -21,9 +22,31 @@ SOURCE_CODE_TARGETS = [
 ]
 
 
+# XXX: I wanted to try some things out.  Don't leave this available.
+@nox.session(python=DEFAULT_PYTHON_VERSION)
+def debug(session: Session) -> None:
+    """Static analysis: lint, formatting, and type checking."""
+    args = session.posargs
+    if len(args) < 1:
+        session.skip("You must provide the tool and command line args.")
+    supported_tools = ["ruff", "mypy"]
+    if args[0] not in supported_tools:
+        session.skip(f"{args[0]} is not one of the supported tools: {supported_tools}")
+
+    try:
+        index = args.index("--targets")
+        _ = args.pop(index)
+        args += SOURCE_CODE_TARGETS
+    except ValueError:
+        pass
+
+    session.log(f"It's running with args: {args}")
+    session.run("poetry", "install", "--with=docs", external=True)
+    session.run(*args)
+
+
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def lint(session: Session) -> None:
-    """Lint using ruff."""
     args = session.posargs or SOURCE_CODE_TARGETS
     session.run("poetry", "install", "--no-root", "--only=test", external=True)
     session.run("ruff", "check", *args)
@@ -31,7 +54,6 @@ def lint(session: Session) -> None:
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def style(session: Session) -> None:
-    """Format code style using ruff (w/ configuration compatible to Black defaults)."""
     args = session.posargs or SOURCE_CODE_TARGETS
     session.run("poetry", "install", "--no-root", "--only=test", external=True)
     session.run("ruff", "format", *args)
@@ -39,15 +61,50 @@ def style(session: Session) -> None:
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def mypy(session: Session) -> None:
-    """Static type checking using mypy."""
     args = session.posargs or SOURCE_CODE_TARGETS
-    session.run("poetry", "install", "--no-root", "--only=test", external=True)
+    session.run("poetry", "install", "--no-root", external=True)
     session.run("mypy", *args)
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
+def lint_plus(session: Session) -> None:
+    """Static analysis: lint, formatting, and type checking."""
+    args = session.posargs or SOURCE_CODE_TARGETS
+    session.debug(args)
+    # session.run("poetry", "install", "--no-root", "--only=test", external=True)
+    session.run("poetry", "install", "--no-root", "--with=nox", external=True)
+    session.run("ruff", "check", *args)
+    session.run("ruff", "format", *args)
+    session.run("mypy", *args)
+
+
+@nox.session(python=DEFAULT_PYTHON_VERSION)
+def docs(session: Session) -> None:
+    """Generate the documentation."""
+    args = session.posargs or ["docs", "docs/_build"]
+    session.run("poetry", "install", "--no-root", "--only=docs", external=True)
+    session.run("sphinx-build", *args)
+
+
+# @nox.session(python=DEFAULT_PYTHON_VERSION)
+# def style(session: Session) -> None:
+#     """Format code style."""
+#     args = session.posargs or SOURCE_CODE_TARGETS
+#     session.run("poetry", "install", "--no-root", "--only=test", external=True)
+#     session.run("ruff", "format", *args)
+
+
+# @nox.session(python=DEFAULT_PYTHON_VERSION)
+# def mypy(session: Session) -> None:
+#     """Static type checking using mypy."""
+#     args = session.posargs or SOURCE_CODE_TARGETS
+#     session.run("poetry", "install", "--no-root", "--only=test", external=True)
+#     session.run("mypy", *args)
+
+
+@nox.session(python=DEFAULT_PYTHON_VERSION)
 def acceptance(session: Session) -> None:
-    """Static type checking using mypy."""
+    """Run acceptance tests."""
     args = session.posargs or ["./tests/features"]
     session.run("poetry", "install", "--only=test", external=True)
     session.run("behave", *args)
--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,3 +1,15 @@
+from typing import Any
+# NOTE: No plans to support type hints in Behave until they drop support for Python2.7.
+#  https://github.com/behave/behave/issues/1168#issuecomment-2028020747
+#
+# For now, I did what I could to bring these, this and the steps/*, modules
+# into compliance with with strict type checking.  See the mypy.ini sections
+# that address errors I could not fix any other way.  For the typehints that I
+# did add here, I started with something very generic and permissive to satisfy
+# the type-checker.  I'd like to come back and find some definitions that are
+# more narrowly scoped.
+
+
 # See: https://behave.readthedocs.io/en/latest/tutorial/#debug-on-error-in-case-of-step-failures
 #
 # $ -D BEHAVE_DEBUG_ON_ERROR
@@ -6,16 +18,16 @@
 BEHAVE_DEBUG_ON_ERROR = False
 
 
-def setup_debug_on_error(userdata):
+def setup_debug_on_error(userdata: Any) -> None:
     global BEHAVE_DEBUG_ON_ERROR
     BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
 
 
-def before_all(context):
+def before_all(context: Any) -> None:
     setup_debug_on_error(context.config.userdata)
 
 
-def after_step(context, step):
+def after_step(context: Any, step: Any) -> None:
     if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
         import ipdb
 
