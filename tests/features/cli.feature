fdceec87a680bccbf55a19ec0db379308e42f4fe
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-22 16:28:06 -0500

    Implement step logic for feature testing

--- tests/features/cli.feature
+++ tests/features/cli.feature
@@ -2,6 +2,7 @@
 Feature: Command Line Interface
 
   Scenario: The CLI is on the binary search path after installation
-    Given the "<application>" application is installed
-    When I execute "<application> --help" in a terminal or shell
-    Then I the application's usage is written to stdout
+    Given the "ProBullStats" application is installed
+    When I execute "probullstats --help" in a terminal or shell
+    Then its usage should be written to stdout
+     and it should indicate success to the executing shell
--- tests/features/steps/cli.py
+++ tests/features/steps/cli.py
@@ -1,23 +1,28 @@
 # ruff: noqa: F811
+import subprocess
+
 from behave import given, when, then
 
 
-@given('the "<application>" application is installed')
+@given('the "ProBullStats" application is installed')
 def step_impl(context):
-    raise NotImplementedError(
-        'STEP: Given the "<application>" application is installed'
-    )
+    try:
+        import probullstats
+    except ImportError as e:
+        raise AssertionError("Is the application installed?") from e
 
 
-@when('I execute "<application> --help" in a terminal or shell')
+@when('I execute "probullstats --help" in a terminal or shell')
 def step_impl(context):
-    raise NotImplementedError(
-        'STEP: When I execute "<application> --help" in a terminal or shell'
-    )
+    context.completed = subprocess.run(args=['probullstats', '--help'], stdout=subprocess.PIPE)
 
 
-@then("I the application's usage is written to stdout")
+@then("its usage should be written to stdout")
 def step_impl(context):
-    raise NotImplementedError(
-        "STEP: Then I the application's usage is written to stdout"
-    )
+    assert context.completed.stdout, "Nothing was written to stdout"
+    context.attach('text/plain', context.completed.stdout)
+
+
+@then("it should indicate success to the executing shell")
+def step_impl(context):
+    assert 0 == context.completed.returncode, f"It indicated an error [{context.completed.returncode}]"
